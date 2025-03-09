2343f21deab721c98b24c194db4ca4e108b34719
jb laptop <43934830+jfrb95@users.noreply.github.com>
2025-03-09 20:33:55 +0000

    begin SectionOutput

---
+++ src/components/FormOutput.jsx
@@ -0,0 +1,15 @@
+import Output from "./Output"
+
+export default function FormOutput({ formData }) {
+    return (
+        <>
+            {formData.map((outputData) => {
+                return (
+                    <Output
+                        
+                    />
+                )
+            })}
+        </>
+    )
+}
---
+++ src/components/SectionOutputs.jsx
@@ -0,0 +1,13 @@
+export default function SectionOutput({ handler }) {
+    return (
+        <>
+            {handler.sectionData.map((formData) => {
+                return (
+                    <FormOutput
+                        
+                    />
+                )
+            })}
+        </>
+    )
+}
