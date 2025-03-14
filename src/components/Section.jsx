b8108f9751ec11298be6acf661651f161ca8a634
Jako <43934830+jfrb95@users.noreply.github.com>
2025-03-14 06:20:44 +0000

    resolve merge conflicts

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,16 +1,11 @@
 import { kebabToTitle } from "../utilityFunctions";
 import Form from "./Form";
 
-<<<<<<< HEAD
-export default function Section({ sectionForms, sectionId, handler }) {
-    console.log(sectionForms);
-=======
 export default function Section({ sectionMap, sectionId, handler }) {
     
     function addFormMap() {
 
     }
->>>>>>> 4f1f69aa31880743126d1fd6011ddb34623ae8d6
     
     return (
         <>
